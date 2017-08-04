d514457846c545449fd3c89b280c9104bb3f4625
Joseph Edwards VIII <jedwards8th@gmail.com>
2017-08-04 10:43:13 -0400

    added deposit.sh

--- collect.sh
+++ collect.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
 main() {
+    [[ ! -e dotfiles ]] && mkdir dotfiles
+    
     # emacs
-    cp -r ~/.emacs.d .
+    cp -r ~/.emacs.d dotfiles
     rm ./.emacs.d/session.*
 
     # screen
-    cp ~/.screenrc .
+    cp ~/.screenrc dotfiles
 
     # bash stuff
-    cp ~/.bash_aliases .
-    cp ~/.bash_profile .
+    cp ~/.bash_aliases dotfiles
+    cp ~/.bash_profile dotfiles
 
     #editorconfig
-    cp ~/.editorconfig .
+    cp ~/.editorconfig dotfiles
 }
 
 main
